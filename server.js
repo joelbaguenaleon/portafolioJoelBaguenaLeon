f6bbd8f04084677b236b565995e791ad0b1db6d2
joelbaguenaleon <joelbagueenaleon@gmail.com>
2026-09-11 00:24:16 +0200

    Servir inicio.html en la ruta raíz

--- server.js
+++ server.js
@@ -2,7 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(__dirname));
+app.use(express.static(__dirname,));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'inicio.html'));
+});
 
 app.listen(process.env.PORT || 3000, () => {
     console.log('Servidor corriendo');
